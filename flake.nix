7b1a05154a01d5597763168b10da89986bac77fd
Gustav Bruhn <gustav.bruhn@live.se>
2024-01-09 22:46:01 +0100

    first commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "My fiest flakes";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager/master";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+  
+  outputs = {self, nixpkgs, home-manager, ...}:
+    let 
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+    nixosConfigurations = {
+      nixos = lib.nixosSystem {
+	inherit system;
+	modules = [./configuration.nix];
+      };
+    };
+    homeConfigurations = {
+      bruhng = home-manager.lib.homeManagerConfiguration {
+        modules = [ ./home.nix ];
+        inherit pkgs;
+      };
+    };
+  };
+}
